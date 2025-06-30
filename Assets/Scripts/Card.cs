e4e7b7d7b3542ed1952977434d658dbfde2b6611
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-06-30 16:06:20 +0900

    Card,Board_Scripts

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    //AudioSource audioSource;
+    //public AudioClip clip;
+
+    public SpriteRenderer frontImage;
+
+    /*
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void Setting(int number)
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }
+
+    public void OpenCard()
+    {
+        if (GameManager.Instance.secondCard != null) return;
+
+        //audioSource.PlayOneShot(clip);
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+        //first가 비었다면
+        if (GameManager.Instance.firstCard == null)
+        {
+            //firstCard에 내 정보를 넘겨준다
+            GameManager.Instance.firstCard = this;
+        }
+        //firstCard가 비어있지 않다면.
+        else
+        {
+            //secondCard에 내 정보를 넘겨준다
+            GameManager.Instance.secondCard = this;
+            //Mached 함수를 호출
+            GameManager.Instance.Matched();
+        }
+    }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 1.0f);
+    }
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 1.0f);
+    }
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+    */
+}
