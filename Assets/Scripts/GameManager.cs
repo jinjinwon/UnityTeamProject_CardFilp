f1f7c5a729eceec026be20292c7362cb8cc4c26d
nakyung71 <nakyung71@naver.com>
2025-06-30 16:09:59 +0900

    GameManagerCommit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+
+{
+    public static GameManager Instance;
+    public Card firstCard;
+    public Card secondCard;
+    public GameObject endTxT;
+    public Text timeTxt;
+    float time = 0.0f;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+    }
+
+    public void Matched();
+    {
+
+    }
+
+}
